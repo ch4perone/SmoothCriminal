8ca42e059d6ab2429ad284bab3dd7635675eb07d
chaperone <chaperone@gmail.com>
2019-05-11 23:13:11 +0100

    Implement Laser

--- Assets/LaserScript.cs
+++ Assets/LaserScript.cs
@@ -11,6 +11,7 @@ public class LaserScript : MonoBehaviour
     void Start()
     {
         line = gameObject.GetComponent<LineRenderer>();
+		line.material.color = Color.red;
 		line.enabled = true;
 
 		
@@ -19,6 +20,23 @@ public class LaserScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+		
+		Ray ray = new Ray(transform.position, transform.forward);
+		line.SetPosition(0, ray.origin);        		
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, 200)) {
+			line.SetPosition(1, hit.point);
+			if(hit.collider.name == "DroneParent") {
+				//TODO loose life or whatever
+				#if UNITY_EDITOR
+					 UnityEditor.EditorApplication.isPlaying = false;
+			 	#else
+					 Application.Quit();
+			 	#endif
+			}
+		}
+		else {
+			line.SetPosition(1, ray.GetPoint(200));
+		}
     }
 }
