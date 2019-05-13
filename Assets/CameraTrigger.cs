79ac80a59bf4c5dd96759dfdcfec67ef7930aac2
chaperone <chaperone@gmail.com>
2019-05-13 13:19:41 +0100

    Camera collision

---
+++ Assets/CameraTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider) {
+		Debug.Log("Enter");
+	}
+
+	void OnTriggerStay(Collider collider) {
+		Debug.Log("Stay");
+	}
+
+	void OnTriggerExit(Collider collider) {
+		Debug.Log("Exit");
+	}
+}
